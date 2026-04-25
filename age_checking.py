e366ec0e50db0fb4ff9bcf7d955cf524f5709952
rishu-karna <ajitdzz6698@gmail.com>
2026-04-25 12:15:04 +0545

    Create age_checking.py

---
+++ age_checking.py
@@ -0,0 +1,6 @@
+a = (int(input("enter your age   ")))
+if (a>18):
+    print ("you can drive car")
+else:
+    print ("you cannot drive car because you are under age")
+print ("Thank you for asking")
